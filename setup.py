d0df64f390db5aa8d8133a5a9ddbc84811a46cf8
Jan Killian <jan.killian@gmail.com>
2015-06-18 11:17:06 +0200

    fix: fix package setup on python 3

--- setup.py
+++ setup.py
@@ -135,7 +135,7 @@ def init(
         if 'debian' in platform.dist():
             try:
                 import stdeb
-            except ImportError, e:
+            except ImportError:
                 pass
 
     return setup
